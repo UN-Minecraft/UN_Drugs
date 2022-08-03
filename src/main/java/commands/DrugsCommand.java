14524dd2993411fdf97832ceaf5ee66ac3f2dc97
Carlos Sandoval <csandovalc@unal.edu.co>
2022-08-03 04:29:34 -0500

    Creacion comando Marihuana

---
+++ src/main/java/commands/DrugsCommand.java
@@ -0,0 +1,36 @@
+package commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import unmineraft.unitems.UNItems;
+import unmineraft.unitems.consumable.Drugs;
+
+public class DrugsCommand implements CommandExecutor {
+    private UNItems plugin;
+
+    public void DrugsCommand(UNItems plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            String errorMessage = ChatColor.RED + " No puedes ejecutar comandos desde la consola";
+            sender.sendMessage(this.plugin.name + errorMessage);
+        }
+
+        Player player = (Player) sender;
+        String message = String.join(" ", args);
+
+        if (message.equalsIgnoreCase("marihuana")){
+            player.getInventory().addItem(Drugs.marihuana);
+        } else {
+            player.sendMessage(ChatColor.DARK_PURPLE + "Elemento no reconocido");
+        }
+
+        return true;
+    }
+}
