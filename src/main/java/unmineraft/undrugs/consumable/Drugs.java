c4c3e8d2c89103cb01e19ed1bfa3b3513013fbc0
Carlos Sandoval <csandovalc@unal.edu.co>
2022-08-06 23:46:21 -0500

    Doc: Se describe de mejor manera el nuevo codigo

--- src/main/java/unmineraft/undrugs/consumable/Drugs.java
+++ src/main/java/unmineraft/undrugs/consumable/Drugs.java
@@ -22,18 +22,33 @@ import java.util.*;
  */
 
 public class Drugs {
-    private static String generalPath = "Config.Drugs";
-    private static List<String> generalLore;
-    private static FileConfiguration config;
+    private static String generalPath = "Config.Drugs"; // Ruta de los items en el archivo de configuracion
+    private static List<String> generalLore; // Descripcion que comparten todos los items
+    private static FileConfiguration config; // Archivo de configuracion
 
-    public static String[] pathDrugs = {"Marihuana", "Perico", "LSD"};
+    public static String[] pathDrugs = {"Marihuana", "Perico", "LSD"}; // Nombre que identifica a cada uno de los items
+
+    /* Estructura de objetos que permite almancenar la informacion de cada elemento en base a nombres, luce algo asi:
+     * drugsInformation = {
+     *    "Marihuana" = {
+     *          "labelEffects" = "Cadena que describe los efectos dados",
+     *          "effectsDuration" = 60 (en segundos),
+     *          "effects" = LinkedList<PotionEffects> (una estructura lineal que almacena todos los efectos de la droga)
+     *     }
+     *    "Perico" = {...}
+     *     ...
+     * }
+     */
     public static HashMap<String, HashMap<String, Object>> drugsInformation = new HashMap<>();
 
-    // Items
+    /* Variables que almacenaran el item creado, esto optimiza el plugin dado que no es necesario construir
+       el mismo elemento repetidas veces, unicamente se ejecuta el proceso una vez y el resultado es almacenado
+       para su clonacion */
     public static ItemStack marihuana;
     public static ItemStack perico;
     public static ItemStack LSD;
 
+    // Permite obtener la lista del archivo de configuracion que se muestran como descripcion de cada item
     private static void updateGeneralLore(String path){
         try {
             List<String> lore = Objects.requireNonNull(config.getStringList(path));
@@ -43,6 +58,8 @@ public class Drugs {
         }
     }
 
+    /* Se verifica que no sea nulo y se "traduce" para que sus colores y estilos sean visibles, posteriormente se muestran
+       en la descripcion del elemento */
     private static String getLabelEffects(String path){
         try {
             String label = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(config.getString(path)));
@@ -52,6 +69,9 @@ public class Drugs {
         }
     }
 
+    /* En minecraft la unidad de medida es el tick, algo similar a una tasa de actualizacion, esta es de
+    *  20 ticks cada segundo, por este motivo es necesario multiplicar los segundos que deseamos que duren los
+    *  efectos y se convierten a una unidad que entienda el juego */
     private static int getDuration(String path){
         int TICKS_PER_SECOND = 20;
         try {
@@ -62,6 +82,7 @@ public class Drugs {
         }
     }
 
+    /* Es necesario crear "pociones", esto se realiza a traves de tener los tipos de efectos, la duracion y el amplificador */
     private static LinkedList<PotionEffect> getEffects(String path, int secondsDuration){
         try {
             LinkedList<PotionEffect> auxListEffects = new LinkedList<>();
@@ -81,6 +102,7 @@ public class Drugs {
         }
     }
 
+    /* Buscamos obtener una cadena no nula y la traducimos para aplicar sus colores y estilos */
     private static String getDisplayName(String path){
         try {
             String plainText = Objects.requireNonNull(config.getString(path));
@@ -90,6 +112,8 @@ public class Drugs {
         }
     }
 
+    /* Almacenamos la informacion recuperada, la almacenamos en un hashmap y posteriormente se almacena dentro de otro
+    *  buscando replicar la estructura ya explicada */
     private static void updateDrugConfig(String pathNameDrug){
         HashMap<String, Object> drug = new HashMap<>();
 
@@ -106,7 +130,7 @@ public class Drugs {
         // Se guarda la informacion en la variable de clase
         drugsInformation.put(pathNameDrug, drug);
     }
-
+    
     protected static ItemStack createItem(Material BaseItem, String pathNameDrug){
         ItemStack item = new ItemStack(BaseItem, 1);
         ItemMeta meta = item.getItemMeta();
@@ -133,6 +157,7 @@ public class Drugs {
         return item;
     }
 
+    /* Metodo general encargado de realizar el proceso de obtencion, configuracion y almacenamiento de los objetos */
     public static void buildDrugs(UNDrugs plugin){
         Drugs.config = plugin.getConfig();
         Drugs.updateGeneralLore(generalPath + ".General_Description");
