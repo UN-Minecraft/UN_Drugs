2e8048154f18e8faa0e4873920999f9997e8b32b
Carlos Sandoval <csandovalc@unal.edu.co>
2022-08-26 13:46:36 -0500

    Add: Manejo de edades y el inventario del jugador
    - Si el bloque se rompe antes de la mayor edad permitida por el juego,
      no entrega nada y envia un mensaje al jugador que indica que aun no
      ha crecido lo suficiente
    - Ahora la entrega se realiza sobre un mismo stack y en caso contrario
      se creara uno si es posible, en caso contrario no se entrega nada y se
      indica que el inventario esta lleno

--- src/main/java/events/BaseItemBreakEvent.java
+++ src/main/java/events/BaseItemBreakEvent.java
@@ -3,11 +3,14 @@ package events;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.scheduler.BukkitRunnable;
 import unmineraft.undrugs.UNDrugs;
 import unmineraft.undrugs.craftBase.BaseItemCraft;
@@ -33,6 +36,16 @@ public class BaseItemBreakEvent implements Listener {
             // Si no es un bloque que necesitemos manejar, finaliza la ejecución
             if (!(isBaseBlockItem.contains(block.getType()))) return;
 
+
+            BlockData dataBlock = block.getBlockData();
+            Ageable ageable = (Ageable) dataBlock;
+
+            if (ageable.getAge() < ageable.getMaximumAge()) {
+                event.getPlayer().sendMessage(plugin.name + ChatColor.WHITE + "Aun no ha crecido lo suficiente");
+                event.setCancelled(true);
+                return;
+            }
+
             Player player = event.getPlayer();
 
             // Cancelamos el dropeo de items
@@ -43,17 +56,49 @@ public class BaseItemBreakEvent implements Listener {
                 @Override
                 public void run(){
                     int indexFirstEmpty = player.getInventory().firstEmpty();
-                    if (indexFirstEmpty == -1){
+
+                    ItemStack item = mapBlockDropItem.get(block.getType());
+                    PlayerInventory playerInventory = player.getInventory();
+
+                    // Si no hay espacio ni un stack disponible
+                    if (indexFirstEmpty == -1 && !(playerInventory.contains(item.getType()))){
                         player.sendMessage(plugin.name + ChatColor.WHITE + "Inventario Lleno");
                         return;
                     }
 
+                    int randomNum = getRandomNumber(1, 4);
                     if (mapBlockDropItem.containsKey(block.getType())){
-                        ItemStack item = mapBlockDropItem.get(block.getType());
+                        // Si el jugador ya tiene el item, sumamos al stack los que acaba de "cultivar"
+                        if (playerInventory.contains(item.getType())){
+                            int slot = playerInventory.first(item.getType());
+                            ItemStack savedItem = playerInventory.getItem(slot);
 
-                        item.setAmount(getRandomNumber(1, 3));
-                        player.getInventory().setItem(indexFirstEmpty, item);
-                        player.sendMessage(plugin.name + ChatColor.WHITE + "Con esto puedes crear tus propias drogas");
+                            if (savedItem == null) return;
+
+                            // Obtenemos el tamaño del stack
+                            int stack = savedItem.getAmount();
+
+                            // Si el stack está lleno creamos otro
+                            if (stack == 64){
+                                item.setAmount(randomNum);
+                                System.out.println(randomNum);
+
+                                // Si no tiene espacio disponible en el stack y no tiene mas espacio no se entrega el item
+                                if (indexFirstEmpty == -1){
+                                    player.sendMessage(plugin.name + ChatColor.WHITE + "Inventario Lleno");
+                                    return;
+                                }
+                                playerInventory.setItem(indexFirstEmpty, item);
+                            } else {
+                                savedItem.setAmount(stack + randomNum);
+                                playerInventory.setItem(slot, savedItem);
+                            }
+
+                        } else {
+                            item.setAmount(randomNum);
+                            player.getInventory().setItem(indexFirstEmpty, item);
+                        }
+                        player.sendMessage(plugin.name + ChatColor.WHITE + "Haz recolectado " + randomNum + " nuevas unidades");
                     }
                 }
             }.run();
